b023b77eb0fc4e3c5c52ae9f5259f24a435661c5
lucasGabrielDeAA <lg.technus@gmail.com>
2021-05-11 15:54:46 -0300

    feat: utils

---
+++ template/src/utils/auth.ts
@@ -0,0 +1,12 @@
+import AsyncStorage from "@react-native-community/async-storage";
+
+import { ASYNC_STORAGE_TOKEN } from "src/constants";
+
+export const getToken = (): Promise<string | null> =>
+  AsyncStorage.getItem(ASYNC_STORAGE_TOKEN);
+
+export const setToken = (token: string): Promise<void> =>
+  AsyncStorage.setItem(ASYNC_STORAGE_TOKEN, token);
+
+export const clearToken = (): Promise<void> =>
+  AsyncStorage.removeItem(ASYNC_STORAGE_TOKEN);
