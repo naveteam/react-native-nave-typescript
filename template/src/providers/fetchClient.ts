41d4cba436ca6acc1cab07a7e32a6eb4a74cfa2a
lucasGabrielDeAA <lg.technus@gmail.com>
2021-05-11 15:55:00 -0300

    feat: providers

---
+++ template/src/providers/fetchClient.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+import { getToken } from "src/utils";
+
+const provider = axios.create({
+  baseURL: "YOUR API URL",
+});
+
+provider.interceptors.request.use(async ({ headers, ...config }) => {
+  const token = await getToken();
+
+  return {
+    ...config,
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+provider.interceptors.response.use(
+  (response) => response?.data,
+  (err) => Promise.reject(err?.response?.data)
+);
+
+export default provider;
