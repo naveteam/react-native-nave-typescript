52050eadc787a80f30e1d5868e956e4031155ab7
William Porto <william.porto@nave.rs>
2021-05-17 12:05:56 -0300

    running prettier script to ensure code style

--- template/src/providers/fetchClient.ts
+++ template/src/providers/fetchClient.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
-import Config from "react-native-config";
+import axios from 'axios';
+import Config from 'react-native-config';
 
-import { getToken } from "src/utils";
+import { getToken } from 'src/utils';
 
 const provider = axios.create({
-  baseURL: Config.API_URL,
+  baseURL: Config.API_URL
 });
 
 provider.interceptors.request.use(async ({ headers, ...config }) => {
@@ -14,14 +14,14 @@ provider.interceptors.request.use(async ({ headers, ...config }) => {
     ...config,
     headers: {
       ...headers,
-      Authorization: token ? `Bearer ${token}` : "",
-    },
+      Authorization: token ? `Bearer ${token}` : ''
+    }
   };
 });
 
 provider.interceptors.response.use(
-  (response) => response?.data,
-  (err) => Promise.reject(err?.response?.data)
+  response => response?.data,
+  err => Promise.reject(err?.response?.data)
 );
 
 export default provider;
