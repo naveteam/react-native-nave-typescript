ee00df5b7c1d16aa2980235a2b2a2c3501f723a7
William Porto <william.porto@nave.rs>
2021-06-10 18:25:30 -0300

    feat(tests): writing withScreen util

--- template/src/utils/tests.tsx
+++ template/src/utils/tests.tsx
@@ -1,8 +1,17 @@
-import React, { ReactElement, FunctionComponent } from 'react';
+import React, { ReactElement, ComponentType, FunctionComponent } from 'react';
 import { render, RenderAPI, RenderOptions } from '@testing-library/react-native';
 import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { theme } from 'src/theme';
+
+interface Screen {
+  name: string;
+  component: ComponentType;
+}
+
+const Stack = createStackNavigator();
 
 const Providers: FunctionComponent = ({ children }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
@@ -10,6 +19,18 @@ const Providers: FunctionComponent = ({ children }) => {
 
 function customRender<T>(ui: ReactElement<T>, options?: RenderOptions): RenderAPI {
   return render(ui, { wrapper: Providers, ...options });
+}
+
+export function withScreen(...screens: Screen[]): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {screens.map(screen => (
+          <Stack.Screen key={screen.name} name={screen.name} component={screen.component} />
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export * from '@testing-library/react-native';
