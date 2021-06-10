08f3049721e02e68a40e3601a663a948d0c61ce7
William Porto <william.porto@nave.rs>
2021-06-10 18:24:53 -0300

    feat(tests): adding mocks to setup-tests

--- template/setup-tests.js
+++ template/setup-tests.js
@@ -1,1 +1,21 @@
 // Here is the place where you should mock all Native Modules before running tests
+import 'react-native-gesture-handler/jestSetup';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+import mockReanimated from 'react-native-reanimated/mock';
+
+global.window = {};
+global.window = global;
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+jest.mock('react-native-reanimated', () => {
+  mockReanimated.default.call = () => {};
+  return mockReanimated;
+});
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('react-native-keyboard-aware-scroll-view', () => {
+  const KeyboardAwareScrollView = ({ children }) => children;
+  return { KeyboardAwareScrollView };
+});
