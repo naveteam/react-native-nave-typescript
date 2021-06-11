c2fa2fc86c7e395e6ddc1c16493a3ed99adbdc76
William Porto <william.porto@nave.rs>
2021-06-11 09:09:40 -0300

    fix(tests): using fake timers to avoid jest warnings log about imports

--- template/setup-tests.js
+++ template/setup-tests.js
@@ -19,3 +19,5 @@ jest.mock('react-native-keyboard-aware-scroll-view', () => {
   const KeyboardAwareScrollView = ({ children }) => children;
   return { KeyboardAwareScrollView };
 });
+
+jest.useFakeTimers();
