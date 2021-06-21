17f1f768482c7e93ad7267a5f83f8b92f75a0ee3
William Porto <william.porto@nave.rs>
2021-06-21 17:53:03 -0300

    fix(android): overriding onConfigurationChanged to notify when device theme has changed

--- template/android/app/src/main/java/com/naverntstemplate/MainActivity.java
+++ template/android/app/src/main/java/com/naverntstemplate/MainActivity.java
@@ -1,8 +1,17 @@
 package com.naverntstemplate;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent;
+import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  public void onConfigurationChanged(Configuration configuration) {
+    super.onConfigurationChanged(configuration);
+    getReactInstanceManager()
+      .onConfigurationChanged(this, configuration);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
