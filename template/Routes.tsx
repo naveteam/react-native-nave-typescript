baff984d0949b2f93deec7fb179e04ba1dfd1489
Michel Silva <michelalmeidasilva@outlook.pt>
2022-07-14 18:58:19 -0300

    adjusts(remove-dependencies): fix code-format

--- template/Routes.tsx
+++ template/Routes.tsx
@@ -8,14 +8,14 @@ import { Home } from 'src/screens';
 import { theme } from 'src/theme';
 
 const ScreenOptions = () => ({
-    cardStyle: { backgroundColor: theme.colors.secondary, paddingHorizontal: 16 },
-    headerShown: false
+  cardStyle: { backgroundColor: theme.colors.secondary, paddingHorizontal: 16 },
+  headerShown: false
 });
 
 const Routes: FC = () => (
-    <Navigator screenOptions={ScreenOptions}>
-        <Screen name='Home' component={Home} />
-    </Navigator>
+  <Navigator screenOptions={ScreenOptions}>
+    <Screen name='Home' component={Home} />
+  </Navigator>
 );
 
 export default Routes;
--- template/src/app-module.tsx
+++ template/src/app-module.tsx
@@ -11,33 +11,33 @@ import { theme } from 'src/theme';
 import Routes from '../Routes';
 
 if (__DEV__) {
-    import('../ReactotronConfig').then(() => console.log('Reactotron Configured'));
+  import('../ReactotronConfig').then(() => console.log('Reactotron Configured'));
 }
 
 const App: FC = () => {
-    const MyTheme: ExtendedTheme = {
-        ...DefaultTheme,
-        colors: {
-            ...DefaultTheme.colors,
-            ...theme.colors
-        }
-    };
+  const MyTheme: ExtendedTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      ...theme.colors
+    }
+  };
 
-    return (
-        <SafeAreaProvider>
-            <NavigationContainer theme={MyTheme}>
-                <Routes />
-            </NavigationContainer>
-        </SafeAreaProvider>
-    );
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer theme={MyTheme}>
+        <Routes />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
 };
 
 export default (): JSX.Element => (
-    <ThemeProvider theme={theme}>
-        <AppProviders>
-            <StatusBar backgroundColor={theme.colors.secondary} barStyle='dark-content' />
+  <ThemeProvider theme={theme}>
+    <AppProviders>
+      <StatusBar backgroundColor={theme.colors.secondary} barStyle='dark-content' />
 
-            <App />
-        </AppProviders>
-    </ThemeProvider>
+      <App />
+    </AppProviders>
+  </ThemeProvider>
 );
