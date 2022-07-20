3fbec3eb96e7aece70d5fb60c41ee4d7cc37b979
Michel Silva <michelalmeidasilva@outlook.pt>
2022-07-20 15:45:32 -0300

    feat(detox): setup detox

---
+++ template/e2e/firstTest.e2e.js
@@ -0,0 +1,12 @@
+const { device, expect, element, by, waitFor } = require('detox');
+
+describe('Example', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  it('should show hello screen after tap', async () => {
+    await element(by.text('Uillia todo poderoso')).tap();
+    await expect(element(by.text('texto do uillia')));
+  });
+});
--- template/src/screens/Home/Home.tsx
+++ template/src/screens/Home/Home.tsx
@@ -1,17 +1,24 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useTheme } from '@react-navigation/native';
 
-import { Column, Row, Text } from 'src/components';
+import { Button, Column, Row, Text } from 'src/components';
 
 const Home: FC = () => {
   const { colors } = useTheme();
+  const [oUillia, setOUillia] = useState('');
 
   return (
     <Column alignItems='center' flex={1} justifyContent='center' p='16px'>
       <Row alignItems='center' mb='10px'>
         <Text color={colors.primary} mr={2}>
-          Built with react-native-nave-typescript
+          {oUillia}
         </Text>
+
+        <Button
+          testID='uillia'
+          text='Uillia todo poderoso'
+          onPress={() => setOUillia('texto do uillia')}
+        />
       </Row>
     </Column>
   );
